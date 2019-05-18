2d42fe8f4b4718433ebc221abfe0cde00f8223c2
tetsuzawa <tetsu.varmos@gmail.com>
2019-05-18 22:21:54 +0900

    add inputrc

--- add_alias.sh
+++ add_alias.sh
@@ -1,5 +1,6 @@
 echo "alias python=python3" >> ~/.bashrc
 echo "alias pip=pip3" >> ~/.bashrc
-echo 'alias la="ls -a"' >> ~/.bashrc
-echo 'alias ll="ls -al"' >> ~/.bashrc
-
+# some more ls aliases
+echo 'alias ll="ls -alF"' >> ~/.bashrc
+echo "alias la='ls -A'" >> ~/.bashrc
+echo "alias l='ls -CF'" >> ~/.bashrc
--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 ln -sf ~/personal_files/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/personal_files/dotfiles/.inputrc ~/.inputrc
