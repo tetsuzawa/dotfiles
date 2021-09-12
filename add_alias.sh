d817068f889f646ff7f0ed066399b3d613895a0b
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2021-09-12 13:41:05 +0900

    delete files

--- add_alias.sh
+++
@@ -1,6 +0,0 @@
-echo "alias python=python3" >> ~/.bashrc
-echo "alias pip=pip3" >> ~/.bashrc
-# some more ls aliases
-echo 'alias ll="ls -alF"' >> ~/.bashrc
-echo "alias la='ls -A'" >> ~/.bashrc
-echo "alias l='ls -CF'" >> ~/.bashrc
--- dotfilesLink.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-ln -sf ~/personal_files/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/personal_files/dotfiles/.inputrc ~/.inputrc
