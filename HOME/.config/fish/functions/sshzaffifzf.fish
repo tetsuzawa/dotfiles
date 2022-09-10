2b724085f379d564795dde13690c2b1aee65a673
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2022-09-10 14:33:01 +0900

    update iroiro

--- HOME/.config/fish/functions/sshzaffifzf.fish
+++ HOME/.config/fish/functions/sshzaffifzf.fish
@@ -1,7 +1,13 @@
-function sshzaffifzf
+function sshfzf
+  set PROFILE $argv[1]
+  if test -z "$PROFILE"
+    set PROFILE "zucks-affiliate"
+  end
+  echo "profile: $PROFILE"
+
   set -l USER te-takizawa
   set -l KEYPATH $HOME/.ssh/id_rsa
-  set -l TARGET (aws --profile zucks-affiliate --region ap-northeast-1 ec2 describe-instances | jq -c '.Reservations[].Instances[] | select(.Tags[].Key == "Name") | select(.State.Name == "running") | {"Name": .Tags[].Value, "InstanceId": .InstanceId, "PublicIpAddress": .PublicIpAddress, "AZ": .Placement.AvailabilityZone}' | fzf --exit-0 | jq -r ".PublicIpAddress")
+  set -l TARGET (aws --profile $PROFILE --region ap-northeast-1 ec2 describe-instances | jq -c '.Reservations[].Instances[] | select(.Tags[].Key == "Name") | select(.State.Name == "running") | {"Name": .Tags[].Value, "InstanceId": .InstanceId, "PublicIpAddress": .PublicIpAddress, "AZ": .Placement.AvailabilityZone}' | fzf --exit-0 | jq -r ".PublicIpAddress")
   if test -n "$TARGET" -a -n "$USER" -a -n "$KEYPATH"
     ssh -o ServerAliveInterval=60 -i $KEYPATH $USER@$TARGET
   end
--- HOME/.config/fish/functions/ssmzgokfzf.fish
+++ HOME/.config/fish/functions/ssmzgokfzf.fish
@@ -1,6 +1,11 @@
-function ssmzgokfzf
-  set -l target (aws --profile zucks-zgok --region ap-northeast-1 ec2 describe-instances | jq -c '.Reservations[].Instances[] | select(.Tags[].Key == "Name") | select(.State.Name == "running") | {"Name": .Tags[].Value, "InstanceId": .InstanceId }' | fzf --exit-0 | jq -r ".InstanceId")
-  if test -n "$target"
-    aws --profile zucks-zgok --region ap-northeast-1 ssm start-session --target $target
+function ssmfzf
+  set PROFILE $argv[1]
+  if test -z "$PROFILE"
+    set PROFILE "zucks-affiliate"
+  end
+  echo "profile: $PROFILE"
+  set -l TARGET (aws --profile $PROFILE --region ap-northeast-1 ec2 describe-instances | jq -c '.Reservations[].Instances[] | select(.Tags[].Key == "Name") | select(.State.Name == "running") | {"Name": .Tags[].Value, "InstanceId": .InstanceId }' | fzf --exit-0 | jq -r ".InstanceId")
+  if test -n "$TARGET"
+    aws --profile $PROFILE --region ap-northeast-1 ssm start-session --target $TARGET
   end
 end
